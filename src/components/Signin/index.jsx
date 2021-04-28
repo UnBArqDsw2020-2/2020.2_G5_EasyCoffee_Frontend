39f7d69ccee1073f8e89594f249c81c7f45cefac
MaiconMares <maiconmaresunb@gmail.com>
2021-04-27 22:15:03 -0300

    Implementando login
    Co-authored-by: Iago <iagotheophilolim@gmail.com>

--- src/components/Signin/index.jsx
+++ src/components/Signin/index.jsx
@@ -1,8 +1,50 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import jwt from "jsonwebtoken";
 
 import './styles.css';
 
 export default function Signin() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        const data = {
+            username,
+            password
+        }
+
+        console.log(data);
+
+        signin(data);
+
+    }
+
+    const signin = async data => {
+        /*fetch('http://localhost:3333/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(data)
+        }).then(response => console.log(response.json()))
+        .catch(error => console.log(error));*/
+
+        const response = await axios.post('http://localhost:3333/api/auth/signin', data);
+        console.log(response.data);
+        
+        const token = await jwt.decode(response.data.accessToken);
+
+        console.log(token);
+
+        localStorage.setItem('@EasyCoffee:token', JSON.stringify(token));
+
+    }
 
     return (
         <div className="master">
@@ -11,12 +53,25 @@ export default function Signin() {
                 <a href="/signup"> Registre-se</a>
             </p>
             <div className="login">
-                <form></form>
-                <label htmlFor="username">Nome de usuário </label>
-                <input type="text" id="username" name="username" required/>
-                <label htmlFor="senha">Senha</label>
-                <input type="text" id="senha" name="senha" required/>
-                <button type="submit" value="Login">Login</button>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Nome de usuário </label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={username}
+                        onChange={event => setUsername(event.target.value)}
+                        required/>
+                    <label htmlFor="senha">Senha</label>
+                    <input 
+                        type="text" 
+                        id="senha" 
+                        name="senha" 
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                        required/>
+                    <button type="submit" value="Login">Login</button>
+                </form>
             </div>
         </div>
     );
